39f84c025d4036d0e8b12bea8a65985d5b5ac39e
Gaëtan <gaetan@moveo-webdesign.com>
2021-03-26 14:50:44 +0100

    feat: add works references popup

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,14 @@
 import './../styles/main.scss'
 
+// SEE WORKS
+//-----------------------------
+const seeWorksButtons = document.querySelectorAll(".js-see-works-button");
+
+seeWorksButtons.forEach($button=>{
+   const buttonID = $button.getAttribute("data-works-button");
+   const $worksPanel = document.querySelector(`[data-works-id="${buttonID}"]`);
+
+    $button.addEventListener("click", ()=>{
+        $worksPanel.classList.toggle("active");
+    })
+});
